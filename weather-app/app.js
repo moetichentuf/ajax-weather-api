335624ea546195d15e581f16a46b5ef76986141e
moeti <moetichentuf@hotmail.com>
2020-01-10 15:55:21 +0100

    works by inputting country (console)

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,26 @@
 (() => {
-    // your code here
+    document.getElementById("run").addEventListener("click", function() {
+    let input = document.getElementById("inp").value;
+    let key = "3003d915e19a0bfa5f7e8f03edbb51b5"; // empty array for multiple elements;
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input+'&appid='+key)
+
+        .then((response)=> {
+            return response.json();
+        })
+
+        .then((data)=> {
+
+
+
+
+
+
+
+                console.log(data)
+         })
+
+      });
 
 
 
