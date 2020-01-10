9bf8872b135876fbcd958359460a67c80cc6ecdc
moeti <moetichentuf@hotmail.com>
2020-01-10 09:51:56 +0100

    first commit

---
+++ weather-app/app.js
@@ -0,0 +1,15 @@
+(() => {
+    // your code here
+    let hero = {}; // empty array for multiple elements;
+    fetch('')
+
+        .then(function (link) {
+            return link.json();
+        })
+
+        .then(function (data) {
+
+
+
+        })
+})();
