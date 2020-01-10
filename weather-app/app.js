f0b46d8159edd9e1d04584f4de1c19757ca9daea
moeti <moetichentuf@hotmail.com>
2020-01-10 11:38:12 +0100

    still trying to figure it out

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,7 @@
 (() => {
     // your code here
-   
-    fetch('')
+let lala =
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=6167865&appid={3003d915e19a0bfa5f7e8f03edbb51b5}')
 
         .then(function (link) {
             return link.json();
@@ -9,7 +9,7 @@
 
         .then(function (data) {
 
-
+console.log(data)
 
         })
 })();
