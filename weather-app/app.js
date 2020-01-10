23d9a6a6ee787c306700d883926b77f5e86e8579
moeti <moetichentuf@hotmail.com>
2020-01-10 12:19:18 +0100

    got the weather to display of one city

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,15 +1,46 @@
 (() => {
     // your code here
-let lala =
-    fetch('https://api.openweathermap.org/data/2.5/weather?id=6167865&appid={3003d915e19a0bfa5f7e8f03edbb51b5}')
 
-        .then(function (link) {
-            return link.json();
-        })
+    const key = '3003d915e19a0bfa5f7e8f03edbb51b5';
+    if(key==='') document.getElementById('temp').innerHTML = ('Remember to add your api key!');
 
-        .then(function (data) {
+    function weatherBallon( cityID ) {
+        fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)
+            .then(function(resp) { return resp.json() }) // Convert data to json
+            .then(function(data) {
+                drawWeather(data);
+            })
+            .catch(function() {
+                // catch any errors
+            });
+    }
 
-console.log(data)
 
-        })
+    function drawWeather( d ) {
+        var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+        var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+        var description = d.weather[0].description;
+
+        document.getElementById('description').innerHTML = description;
+        document.getElementById('temp').innerHTML = celcius + '&deg;';
+        document.getElementById('location').innerHTML = d.name;
+
+        if( description.indexOf('rain') > 0 ) {
+            document.body.className = 'rainy';
+        } else if( description.indexOf('cloud') > 0 ) {
+            document.body.className = 'cloudy';
+        } else if( description.indexOf('sunny') > 0 ) {
+            document.body.className = 'sunny';
+        } else {
+            document.body.className = 'clear';
+        }
+    }
+
+    window.onload = function() {
+        weatherBallon( 6167865 );
+    }
+
+
+
+
 })();
