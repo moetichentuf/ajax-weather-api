f696912d9e805660eb5dc25853fba0fbb5944797
moeti <moetichentuf@hotmail.com>
2020-01-10 16:00:57 +0100

    added metric

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,15 +1,15 @@
 (() => {
     document.getElementById("run").addEventListener("click", function() {
-    let input = document.getElementById("inp").value;
-    let key = "3003d915e19a0bfa5f7e8f03edbb51b5"; // empty array for multiple elements;
+        let input = document.getElementById("inp").value;
+        let key = "3003d915e19a0bfa5f7e8f03edbb51b5"; // empty array for multiple elements;
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input+'&appid='+key)
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='+input+'&appid='+key+"&units=metric")
 
-        .then((response)=> {
-            return response.json();
-        })
+            .then((response)=> {
+                return response.json();
+            })
 
-        .then((data)=> {
+            .then((data)=> {
 
 
 
@@ -18,9 +18,9 @@
 
 
                 console.log(data)
-         })
+            })
 
-      });
+    });
 
 
 
